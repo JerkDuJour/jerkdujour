3420510d9cf9b61a8596e98daaa481184d71ba06
Brian Shef <brianshef@gmail.com>
2015-03-24 11:20:22 -0500

    Added more fleshed out sample reports in a dictionary

--- index.js
+++ index.js
@@ -2,21 +2,9 @@ var express = require('express');
 var app = express();
 app.set('port', (process.env.PORT || 5000));
 
-// RESTful API
-app.get('/', function(req, res) {
-    res.send('Welcome to Jerk du Jour!');
-});
-app.get('/reports', function(req, res) {
-    res.send([{name:'report1'}, {name:'report2'}]);
-});
-app.get('/reports/:id', function(req, res) {
-    res.send({id:req.params.id, name: "The Name", description: "description"});
-});
-
-// Sample report JSON data
-/*
-app.get('/abcd1234', function(request, response) {
-  var jsonObj = {
+// Sample reports
+var reports = {};
+var report1 = {
     'id': 'abcd1234',
     'type': 'jerk_report',
     'utc_time': '2014-11-05T13:15:30Z',
@@ -24,12 +12,33 @@ app.get('/abcd1234', function(request, response) {
     'offense': 'double_parked',
     'notes': 'Black BMW i3'
   };
+ var report2 = {
+    'id': 'efgh5678',
+    'type': 'jerk_report',
+    'utc_time': '2014-12-16T22:08:35Z',
+    'reportee_id': 'xray802',
+    'offense': 'talking_at_urinal',
+    'notes': ''
+  };
+reports['abcd1234'] = report1
+reports['efgh5678'] = report2
 
-  // Return result as a string for now
-  result = JSON.stringify(jsonObj);
-  response.send(result);
+// RESTful API
+app.get('/', function(req, res) {
+    res.send('Welcome to Jerk du Jour API!');
 });
-*/
+app.get('/reports', function(req, res) {
+    res.send(reports);
+});
+app.get('/reports/:id', function(req, res) {
+    if(id:req.params.id in reports){
+        report = reports[id:req.params.id]
+        res.send(report)
+    }
+    else {
+        res.send('A report with ID ' + id:req.params.id + ' does not exist.')
+    }
+});
 
 //
 // START THE SERVER
